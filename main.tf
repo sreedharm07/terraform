bb690581f93a0b0784f87fa37242bb40036ceb48
sreedhar <sreedhar70755@gmail.com>
2023-11-17 17:20:26 +0530

    tf

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ module "vpc" {
   source   = "git::https://github.com/sreedharm07/infra-vpc.git"
   for_each = var.vpc
 
-  cidr     = each.value["cidr"]
+  cidr    = each.value["cidr"]
+  subnets = each.value["subnets"]
 }
